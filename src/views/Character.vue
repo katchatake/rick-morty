20d18483fa0d623d46f78cb6996067cb9f6c3467
katchatake <frangarmu95@gmail.com>
2020-12-10 18:01:02 -0600

    integracion de la vista character

---
+++ src/views/Character.vue
@@ -0,0 +1,11 @@
+<template>
+  <Character />
+</template>
+
+<script>
+import Character from "@/components/CharacterComponent.vue";
+export default {
+  name: "Character",
+  components: { Character },
+};
+</script>
